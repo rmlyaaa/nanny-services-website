eb1a37665db782d4f83672e5c70aadc9d3151b57
rmlyaaa <126497199+rmlyaaa@users.noreply.github.com>
2024-09-05 21:24:58 +0200

    homepage container fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,23 @@ import homeImg from "/home-img.png";
 
 const Home = () => {
   return (
-    <div className="mx-5 p-0 row container-lg align-items-center justify-content-center bg-darkgreen rounded-4">
-      <div className="col-6 p-5 ">
-        <h1 className="mb-3 text-white ">Make Life Easier for the Family:</h1>
-        <h6 className="mb-4 text-white">
-          Find Babysitters Online for All Ocassions
-        </h6>
-        <button
-          type="button"
-          className="btn mt-2 px-5 py-2 border border-1 rounded-pill text-white"
-        >
-          Get started <i className="bi bi-arrow-up-right"></i>
-        </button>
-      </div>
-      <div className="col-6 p-0">
-        <img src={homeImg} alt="home image" className="img-fluid" />
+    <div className="d-flex justify-content-center">
+      <div className="mx-5 p-0 row container-lg align-items-center bg-darkgreen rounded-4">
+        <div className="col-6 p-5 ">
+          <h1 className="mb-3 text-white ">Make Life Easier for the Family:</h1>
+          <h6 className="mb-4 text-white">
+            Find Babysitters Online for All Ocassions
+          </h6>
+          <button
+            type="button"
+            className="btn mt-2 px-5 py-2 border border-1 rounded-pill text-white"
+          >
+            Get started <i className="bi bi-arrow-up-right"></i>
+          </button>
+        </div>
+        <div className="col-6 p-0">
+          <img src={homeImg} alt="home image" className="img-fluid" />
+        </div>
       </div>
     </div>
   );
